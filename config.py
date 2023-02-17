e4f9449a00bf5e889748ee904786fa8d11558e30
Mostafa Asadi <mostafaasadi73@gmail.com>
2023-02-17 15:43:02 +0330

    add config

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+
+class Config:
+    load_dotenv()
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+    WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    HOST = '0.0.0.0'
+    PORT = '1211'
+
+class Development(Config):
+    DEBUG = True
+    DEVELOPMENT = True
+
+
+class Production(Config):
+    DEBUG = False
